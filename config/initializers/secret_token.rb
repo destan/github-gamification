9acc4b1f3f484bd552dda8bcfad0754c2e772096
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-24 21:31:41 +0300

    rails new github-gamification

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubGamification::Application.config.secret_key_base = '76cc8f2d19653006dbcd56fe44adde2d32296430af970f6baa5ceb1a62e1e8f6d1ccebf7a7f3a814cc256ae533949c4182eddf9ed4c072add14fd18e5a716188'
