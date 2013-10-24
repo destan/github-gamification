b288ba98181e93d820f4355992bda9aee222b662
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-24 22:23:44 +0300

    CommitWorker is created

---
+++ app/workers/commit_worker.rb
@@ -0,0 +1,18 @@
+class CommitWorker
+  @queue = :commit_queue
+
+  def self.perform(commit, repo, repo_owner)
+    commit_details = $gdp.get_commit(commit["sha"], repo, repo_owner)
+    files = commit_details.files
+
+    files.each do |file|
+      commit_file = CommitFile.new
+      commit_file.filename = file.filename
+      commit_file.additions = file.additions
+      commit_file.deletions = file.deletions
+      #commit_file.changs = commit_file.changes
+      commit_file.commit_id  = commit["id"]
+      commit_file.save
+    end
+  end
+end
