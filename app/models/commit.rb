fb2e28bb7d8b8667ccbb2eec7ebc1fd6ae8c7733
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-26 16:46:44 +0300

    destroy dependencies are added to Repo and Commit models

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,7 +1,7 @@
 class Commit < ActiveRecord::Base
   belongs_to :repo
-  has_many :commit_files
-  has_many :commit_points
+  has_many :commit_files, :dependent => :destroy
+  has_many :commit_points, :dependent => :destroy
   has_many :languages, :through => :commit_points
 
   validates :sha, :uniqueness => true
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,6 +1,6 @@
 class Repo < ActiveRecord::Base
   belongs_to :github_account
-  has_many :commits
+  has_many :commits, :dependent => :destroy
   has_many :commit_files, :through => :commits
 
   validates :repo_id, uniqueness: true
