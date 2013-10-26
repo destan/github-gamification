3cc3e14e23404def5a9c29d02869281996c4ca2e
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-26 21:42:45 +0300

    Language seed is added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+languages = [
+  {
+    name: "Ruby",
+    extensions: [".rb", ".erb"],
+    ratio: 1.7
+  },
+  {
+    name: "Javascript",
+    extensions: [".js"],
+    ratio: 1.3
+  },
+  {
+    name: "CSS",
+    extensions: [".css"],
+    ratio: 1
+  },
+  {
+    name: "HTML",
+    extensions: [".html"],
+    ratio: 1
+  },
+  {
+    name: "C",
+    extensions: [".c"],
+    ratio: 2.2
+  },
+  {
+    name: "C++",
+    extensions: [".cpp"],
+    ratio: 2.1
+  },
+  {
+    name: "PHP",
+    extensions: [".php"],
+    ratio: 1.5
+  },
+  {
+    name: "C#",
+    extensions: [".cs"],
+    ratio: 1.3
+  },
+  {
+    name: "ASP",
+    extensions: [".asp", ".aspx"],
+    ratio: 1.4
+  },
+  {
+    name: "Java",
+    extensions: [".java", ".jav", ".j"],
+    ratio: 1.6
+  },
+]
+
+languages.each { |l| Language.create({name: l[:name], extensions: l[:extensions], ratio: l[:ratio]}) }
