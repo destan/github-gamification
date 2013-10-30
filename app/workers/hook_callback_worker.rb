e47d5683722683597f4e65de106cba3bcd06f914
Mehmet Beydogan <mbbrain@gmail.com>
2013-10-30 23:47:11 +0200

    HookCallbackWorker is created

---
+++ app/workers/hook_callback_worker.rb
@@ -0,0 +1,26 @@
+class HookCallbackWorker
+  @queue = :hook_callback_queue
+
+  def self.perform(repo, commits)
+    repo_obj = Repo.where("repo_id = ?", repo["id"]).first
+
+    commits.each do |commit|
+      login = commit["author"]["username"]
+      github_account = GithubAccount.find_by_login(login)
+
+
+      if github_account
+        commit_obj = Commit.new({
+          sha: commit["id"],
+          message: commit["message"],
+          url: commit["url"],
+          repo_id: repo_obj.id,
+          github_account_id: github_account.id
+        })
+        Resque.enqueue(CommitWorker, ) if commit_obj.save
+      end
+
+    end
+
+  end
+end
